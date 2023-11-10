da0e219e477ddda18967e423e04d23171481ca00
don't write <111179290+dontwriteme@users.noreply.github.com>
2023-11-10 21:03:42 +0700

    Update hangman.py

--- hangman_unfinished.py
+++ hangman_unfinished.py
@@ -1,88 +1,91 @@
 import random
 
 stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
- ========= 
- ''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
- ========= 
- ''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
- ========= 
- ''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
- ========= 
- ''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
- ========= 
-  ''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
- ========= 
- ''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
- ========= 
- ''']
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+========== 
+''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+========== 
+''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+==========
+''', '''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+========== 
+''', '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+========== 
+''', '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+========== 
+''', '''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+========== ''']
 
-word_list = ['palm','brest','knuckle']
-
+word_list = ['breast', 'chin', 'shoulder']
 chosen_word = random.choice(word_list)
 
-print(chosen_word)
-
-blanks_list = []
-
+display = []
 for i in chosen_word:
-    blanks_list += '_'
+    display += '_'
+print(display)
 
-end_of_game = False
+end_of_game = 0
 lives = 6
 
-while not end_of_game:
- letter = input('Choose a letter: ').lower()
+while end_of_game == 0:
+    selected_letter = input('Select a letter: ').lower()
 
- for i in range(len(chosen_word)):
-    index_letter = chosen_word[i]
-    if index_letter == letter:
-        blanks_list[i] = index_letter
-    
- print(blanks_list)
- print(lives)
+    for i in range(len(chosen_word)):
+        index_letter = chosen_word[i]
 
- if '_' not in blanks_list:
-     end_of_game = True
-     print('You win!')
+        if index_letter == selected_letter:
+            display[i] = index_letter
+    print(display)
+
+    if selected_letter not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = 1
+        print(f'Lose one live! You have {lives} lives')
+
+    if '_' not in display:
+        end_game = 1
+        print('You win!')
+
+    print(stages[lives])
